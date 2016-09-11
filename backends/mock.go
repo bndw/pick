14cd233607461b5fd64eae97620b99a0668394dc
bndw <benjamindwoodward@gmail.com>
2016-09-11 09:23:01 -0700

    use new safe format

--- backends/mock.go
+++ backends/mock.go
@@ -6,14 +6,13 @@ type mockBackend struct {
 }
 
 func NewMockBackend() *mockBackend {
-	safeData := []byte(`-----BEGIN PGP SIGNATURE-----
-
-wx4EBwMIEp9YTGqqetBgwXj5+80vLdBMeIvLS49/GPDS4AHkVDeGlKy/bXalpl9M
-q7YwLOEBo+DO4CTh9ujgVeKriVie4HjmoykOiH3REZLAJO7U3ejMIP4onlD0u6SJ
-vD4U2ipsQIWhWaWfASmpl6T0Qq39tDqE8XZjFcTt/Btujfb5zoPpbeA84s53sEXg
-suDC4K7k5IYOU66CG3XbOaafkrrfmeKCjJGG4aZyAA==
-=Wzqv
------END PGP SIGNATURE-----`)
+	safeData := []byte(`-----BEGIN PGP MESSAGE-----
+wx4EBwMI/EyvqWA12cNgJBnoGRxYO1D0/F/w5Ro5uafS4AHkLjgl3wFVjIRB1vbo
+GSX6FeE9q+Ap4JzhoTTgcOLB6iyW4HDmGZFzcVq+JgYYg0+7Q+4jlC/bBxyhtb1h
+UHBuCvFGG4ENExdLliCsixI1bP8KB2TlLH459U859KWkg1aEJJ+1FeDR5E1GwV5y
+Jn766KqjJFAUxwvguuNHI0fMMcIyfeA+4uNDsmXg+uRsGhwVdCP509FRtqes0EPh
+4mqkkV7hFAgA=geI2
+-----END PGP MESSAGE-----`)
 
 	return &mockBackend{data: safeData}
 }
