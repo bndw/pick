c4982d05adb6fcdd730c5cfb4bed5ca894f7b2cb
bndw <benjamindwoodward@gmail.com>
2016-09-15 06:59:59 -0700

    Cleanup variable scope

--- src/safe/replace.go
+++ src/safe/replace.go
@@ -1,15 +1,14 @@
 package safe
 
 func (s *Safe) Replace(name, username, password string) (*Account, error) {
-	account, err := s.Get(name)
-	if err != nil {
+	if _, err := s.Get(name); err != nil {
 		return nil, err
 	}
 
-	account = NewAccount(name, username, password)
+	account := NewAccount(name, username, password)
 	s.Accounts[name] = *account
 
-	if err = s.save(); err != nil {
+	if err := s.save(); err != nil {
 		return nil, err
 	}
 
