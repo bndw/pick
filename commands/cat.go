3aaa70f1ff06f848890208e8164b47c43f43445a
Leon Klingele <git@leonklingele.de>
2017-01-02 12:30:16 +0100

    Add --history flag for 'cat' command to show an account's full history

--- commands/cat.go
+++ commands/cat.go
@@ -4,20 +4,53 @@ import (
 	"fmt"
 
 	"github.com/bndw/pick/errors"
+	"github.com/bndw/pick/safe"
 	"github.com/bndw/pick/utils"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
 )
 
 func init() {
-	rootCmd.AddCommand(&cobra.Command{
+	cmd := &cobra.Command{
 		Use:   "cat [name]",
 		Short: "Cat a credential",
 		Long:  "The cat command is used to print a credential to stdout.",
 		Run: func(cmd *cobra.Command, args []string) {
 			runCommand(Cat, cmd, args)
 		},
-	})
+	}
+	cmd.Flags().Bool("history", false, "show credential history")
+	rootCmd.AddCommand(cmd)
+}
+
+func printAccount(name string, account *safe.Account, showHistory bool) {
+	// Print header
+	if showHistory && account.History != nil {
+		history := account.History
+		account.History = nil
+		history = append(history, *account)
+		for i, l := 0, len(history); i < l; i++ {
+			printCredential(&history[i], "  ", i == 0)
+		}
+	} else {
+		// Print a credential
+		printCredential(account, "", true)
+	}
+}
+
+func printCredential(account *safe.Account, printPrefix string, isInitialAccount bool) {
+	// Print a credential
+	var createdOrModified string
+	if isInitialAccount {
+		createdOrModified = "created"
+	} else {
+		createdOrModified = "modified"
+	}
+	fmt.Printf("%s: %s\n%susername: %s\n%spassword: %s\n",
+		createdOrModified,
+		utils.FormatUnixTime(account.ModifiedOn),
+		printPrefix, account.Username,
+		printPrefix, account.Password)
 }
 
 func Cat(args []string, flags *pflag.FlagSet) error {
@@ -26,23 +59,26 @@ func Cat(args []string, flags *pflag.FlagSet) error {
 	}
 	name := args[0]
 
-	safe, err := loadSafe()
+	showHistory, err := parseCatFlags(flags)
 	if err != nil {
 		return err
 	}
 
-	account, err := safe.Get(name)
+	s, err := loadSafe()
 	if err != nil {
 		return err
 	}
 
-	fmt.Printf(`account:  %s
-username: %s
-password: %s
-created:  %s
-modified: %s
-`, name, account.Username, account.Password,
-		utils.FormatUnixTime(account.CreatedOn),
-		utils.FormatUnixTime(account.ModifiedOn))
+	account, err := s.Get(name)
+	if err != nil {
+		return err
+	}
+
+	printAccount(name, account, showHistory)
 	return nil
+}
+
+func parseCatFlags(flags *pflag.FlagSet) (showHistory bool, err error) {
+	showHistory, err = flags.GetBool("history")
+	return
 }
